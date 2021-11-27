52d12de290b6458889b9c41a105893c5161beb5c
Rohit Prasad <rohitpd8101@gmail.com>
2021-11-27 22:06:22 +0530

    inital commit

---
+++ index.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class index{
+
+    public static void main(String[] args) {
+
+        int n=5;
+        System.out.println(4+n);
+        system.out.println(1+2);
+
+        int sadf=63;
+    }
+}
